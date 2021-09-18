1ddf83e0718eb52c44f36dd6fcdde8bf1cc51869
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-09-18 20:49:43 +0300

    update

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Mel from '../images/Mel_pic.jpeg'
+
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <img src= {Mel} alt="pic" className="pic"></img>
+        <ul className="menu">
+          <li><a href="/">About</a></li>
+          <li><a href="/#education">Education</a></li>
+          <li><a href="/#portfolio">Portfolio</a></li>
+          <li><a href="/#contact">Contact</a></li>
+        </ul>
+      </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
